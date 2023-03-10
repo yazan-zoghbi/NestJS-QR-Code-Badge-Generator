ceab609ede3795d6dc1544d6230c3d472c8fdd13
Yazan Zoghbi <yazan.a.zoghbi@gmail.com>
2023-03-10 19:44:47 +0300

    [Chore] customer schema improved

--- src/modules/customer/schema/customer.schema.ts
+++ src/modules/customer/schema/customer.schema.ts
@@ -1,22 +1,25 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import mongoose, { Document } from 'mongoose';
-import { QrCode } from 'src/modules/qr/schemas/qr.schema';
+import { QrBadge } from 'src/modules/qr/schemas/qr.schema';
 
 export type CustomerDocument = Customer & Document;
 
 @Schema()
 export class Customer {
-  @Prop({unique: true})
+
+  @Prop({ unique: true })
   username: string;
 
-  @Prop({ unique: true, lowercase:true })
+  @Prop({ unique: true, lowercase: true })
   email: string;
 
   @Prop()
-  password: string
+  password: string;
 
-  @Prop({type: [{type: mongoose.Schema.Types.ObjectId, ref:'QrCode'}]})
-  qr_code_id: QrCode[]
+  @Prop({
+    type: [{ type: [{type: mongoose.Schema.Types.ObjectId, ref: 'QrCode' }]}],
+  })
+  qr_code_id?: QrBadge[];
 }
 
 export const CustomerSchema = SchemaFactory.createForClass(Customer);
