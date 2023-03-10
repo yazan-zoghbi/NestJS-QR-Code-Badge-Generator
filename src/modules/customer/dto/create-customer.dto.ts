ab5f8219034be6d6073e9b2e09978f39604756d2
Yazan Zoghbi <yazan.a.zoghbi@gmail.com>
2023-03-10 19:44:06 +0300

    [Add] create customer dto validation added

---
+++ src/modules/customer/dto/create-customer.dto.ts
@@ -0,0 +1,15 @@
+import { IsString, IsEmail, IsNotEmpty } from 'class-validator';
+
+export class CreateCustomerDto {
+  @IsNotEmpty()
+  @IsString()
+  readonly username: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  readonly email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  readonly password: string;
+}
