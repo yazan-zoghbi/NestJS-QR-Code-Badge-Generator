a37f491295d4b44d7d3d33ab808ea1dbf3286a37
Yazan Zoghbi <yazan.a.zoghbi@gmail.com>
2023-03-13 05:25:45 +0300

    [Chore] name property type changed to QRTypeEnum.

--- src/modules/qr/schemas/qr.type.schema.ts
+++ src/modules/qr/schemas/qr.type.schema.ts
@@ -1,12 +1,13 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
+import { QRTypeEnum } from '../qr-type.enum';
 
 export type QrTypeDocument = QrType & Document;
 
 @Schema()
 export class QrType {
-  @Prop()
-  name: string;
+  @Prop({ enum: Object.values(QRTypeEnum), required: true })
+  name: QRTypeEnum;
 }
 
 export const QrTypeSchema = SchemaFactory.createForClass(QrType);
