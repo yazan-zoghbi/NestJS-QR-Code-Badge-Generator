202b7303e60d19700acd9857834bf9b4122d3855
Yazan Zoghbi <yazan.a.zoghbi@gmail.com>
2023-03-13 05:22:40 +0300

    [Chore] new types defined for properties.

--- src/modules/qr/dto/create-qr.dto.ts
+++ src/modules/qr/dto/create-qr.dto.ts
@@ -1,6 +1,18 @@
+import { CustomerData } from '../customer-data.interface';
+import { GenerateQRDataService } from 'src/shared/generate-data/generate-qr-data.service';
+import { QRTypeEnum } from '../qr-type.enum';
+
 export class CreateQrDto {
-    readonly data: string;
-    readonly customer_id: string;
-    readonly type_id: string;
+  readonly customerData: CustomerData;
+  readonly customer_id: string;
+  readonly type: QRTypeEnum;
+
+  constructor(private readonly generateQRDataService: GenerateQRDataService) {}
+  get data(): string {
+    return this.generateQRDataService.generate(
+      this.type,
+      this.customerData,
+      this.customer_id,
+    );
   }
-  
+}
