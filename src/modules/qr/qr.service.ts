33698a6e0479c65f159e7dbe6821655bf61cdb00
Yazan Zoghbi <yazan.a.zoghbi@gmail.com>
2023-03-15 02:25:36 +0300

    [Fix] QR code generated data not readable fixed.

--- src/modules/qr/qr.service.ts
+++ src/modules/qr/qr.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import * as QRCode from 'qrcode';
@@ -6,6 +6,8 @@ import { CreateQrDto } from './dto/create-qr.dto';
 import { QrBadge } from './schemas/qr.schema';
 import { QrRepository } from './qr.repository';
 import { CustomerRepository } from '../customer/customer.repository';
+import { QRTypeEnum } from './qr-type.enum';
+import { CustomerData, VCardData } from './customer-data.interface';
 
 @Injectable()
 export class QrService {
@@ -15,10 +17,29 @@ export class QrService {
     private readonly customerRepository: CustomerRepository,
   ) {}
 
+  async generateData(
+    qrType: QRTypeEnum,
+    customerData: CustomerData | VCardData,
+    customer_id: string,
+  ) {
+    switch (qrType) {
+      case QRTypeEnum.VCard:
+        const VCardData = customerData as VCardData;
+        const data = `BEGIN:VCARD\nVERSION:2.1\nN:${VCardData.lastName};${VCardData.firstName}\nFN:${VCardData.firstName} ${VCardData.lastName}\nTEL:${VCardData.phoneNumber}\nEMAIL:${VCardData.email}\nEND:VCARD`;
+        return data;
+
+      default:
+        throw new BadRequestException('Invalid QR type');
+    }
+  }
+
   async generateQrCode(createQrDto: CreateQrDto): Promise<QrBadge> {
-    const qrCodeData = await QRCode.toDataURL(createQrDto.data);
-    const qrCodeImage = await QRCode.toBuffer(createQrDto.data);
-    const qrType = createQrDto.type
+    const { type, customerData, customer_id } = createQrDto;
+    const data = await this.generateData(type, customerData, customer_id);
+    const qrCodeData = await QRCode.toDataURL(data);
+    const qrCodeImage = await QRCode.toBuffer(data);
+    const qrType = createQrDto.type;
+    const qrTypeid = await this.qrRepository.findQRTypeByName(qrType);
 
     const customer = await this.customerRepository.findById(
       createQrDto.customer_id,
@@ -28,7 +49,7 @@ export class QrService {
     qrCode.data = qrCodeData;
     qrCode.image = qrCodeImage;
     qrCode.customer_id = customer;
-    qrCode.type = qrType;
+    qrCode.type = qrTypeid;
 
     const createdQrCode = await this.qrRepository.createQrCode(qrCode);
 
