bba108c3f1349b2b79c169e7cf49533332b0e11e
Yazan Zoghbi <yazan.a.zoghbi@gmail.com>
2023-03-15 02:24:06 +0300

    [Chore] validation added to check DTO object.

--- src/modules/qr/qr.controller.ts
+++ src/modules/qr/qr.controller.ts
@@ -1,17 +1,24 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, BadRequestException } from '@nestjs/common';
 import { QrService } from './qr.service';
 import { CreateQrDto } from './dto/create-qr.dto';
 import { QrTypeService } from './qr.type.service';
+import { validate } from 'class-validator';
 
 @Controller('qr')
 export class QrController {
   constructor(
     private readonly qrService: QrService,
     private readonly qrTypeService: QrTypeService,
+
   ) {}
 
   @Post('/generate')
   async generateQrCode(@Body() createQrDto: CreateQrDto) {
+    const errors = await validate(createQrDto);
+    if (errors.length > 0) {
+      throw new BadRequestException(errors);
+    }
+      
     const qrCode = await this.qrService.generateQrCode(createQrDto);
     return qrCode;
   }
