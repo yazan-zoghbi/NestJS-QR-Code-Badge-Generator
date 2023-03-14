a47004832815240a911ece65456abe3338504cea
Yazan Zoghbi <yazan.a.zoghbi@gmail.com>
2023-03-15 02:27:01 +0300

    [Fix] type property reference fixed.

--- src/modules/qr/schemas/qr.schema.ts
+++ src/modules/qr/schemas/qr.schema.ts
@@ -1,7 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import mongoose, { Document } from 'mongoose';
 import { Customer } from 'src/modules/customer/schema/customer.schema';
-import { QRTypeEnum } from '../qr-type.enum';
 import { QrType } from './qr.type.schema';
 
 export interface QrBadge {
@@ -9,7 +8,7 @@ export interface QrBadge {
   data: string;
   image: Buffer;
   customer_id: Customer;
-  type: QRTypeEnum;
+  type: QrType;
 }
 
 export type QrCodeDocument = QrBadge & Document;
@@ -26,7 +25,7 @@ export class QrBadge {
   customer_id: Customer;
 
   @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'QrType' })
-  type: QRTypeEnum;
+  type: QrType;
 }
 
 export const QrCodeSchema = SchemaFactory.createForClass(QrBadge);
